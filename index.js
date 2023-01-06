b13c8214cb8e67325ed1dc774844fb0eea42d5f3
MyEcoria <103332030+MyEcoria@users.noreply.github.com>
2023-01-06 17:09:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -92,3 +92,81 @@ function getOrder(id) {
     console.log(body);
   });
 }
+
+function getCurrenciesPartner() {
+  const options = {
+    method: 'GET',
+    url: 'https://api.nanswap.com/v1/get-currencies-partner',
+    json: true
+  };
+
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+
+    console.log(body);
+  });
+}
+
+function getEstimatePartner(from, to, amount, fromNetwork, toNetwork) {
+  const options = {
+    method: 'GET',
+    url: `https://api.nanswap.com/v1/get-estimate-partner?from=${from}&to=${to}&amount=${amount}&fromNetwork=${fromNetwork}&toNetwork=${toNetwork}`,
+    json: true
+  };
+
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+
+    console.log(body);
+  });
+}
+
+function getLimitsPartner(from, to, fromNetwork, toNetwork) {
+  const options = {
+    method: 'GET',
+    url: `https://api.nanswap.com/v1/get-limits-partner?from=${from}&to=${to}&fromNetwork=${fromNetwork}&toNetwork=${toNetwork}`,
+    json: true
+  };
+
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+
+    console.log(body);
+  });
+}
+
+function createOrderPartner(from, fromNetwork, to, toNetwork, amount, toAddress) {
+  const options = {
+    method: 'POST',
+    url: 'https://api.nanswap.com/v1/create-order-partner',
+    body: {
+      from: from,
+      fromNetwork: fromNetwork,
+      to: to,
+      toNetwork: toNetwork,
+      amount: amount,
+      toAddress: toAddress
+    },
+    json: true
+  };
+
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+
+    console.log(body);
+  });
+}
+
+function getOrderPartner(id) {
+  const options = {
+    method: 'GET',
+    url: `https://api.nanswap.com/v1/get-order-partner?id=${id}`,
+    json: true
+  };
+
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+
+    console.log(body);
+  });
+}
