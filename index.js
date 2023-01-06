07d15b26da62f87afb8798beb7a8f171f3a68009
MyEcoria <103332030+MyEcoria@users.noreply.github.com>
2023-01-06 15:29:29 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,94 @@
+const request = require('request');
+
+function getCurrencies() {
+  const options = {
+    method: 'GET',
+    url: 'https://api.nanswap.com/v1/get-currencies',
+    json: true
+  };
+
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+
+    console.log(body);
+  });
+}
+
+function getEstimate(from, to, amount) {
+  const options = {
+    method: 'GET',
+    url: `https://api.nanswap.com/v1/get-estimate?from=${from}&to=${to}&amount=${amount}`,
+    json: true
+  };
+
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+
+    console.log(body);
+  });
+}
+
+function getEstimateReverse(from, to, amount) {
+  const options = {
+    method: 'GET',
+    url: `https://api.nanswap.com/v1/get-estimate-reverse?from=${from}&to=${to}&amount=${amount}`,
+    json: true
+  };
+
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+
+    console.log(body);
+  });
+}
+
+function getLimits(from, to) {
+  const options = {
+    method: 'GET',
+    url: `https://api.nanswap.com/v1/get-limits?from=${from}&to=${to}`,
+    json: true
+  };
+
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+
+    console.log(body);
+  });
+}
+
+function createOrder(apiKey, from, to, amount, toAddress) {
+  const options = {
+    method: 'POST',
+    url: 'https://api.nanswap.com/v1/create-order',
+    headers: {
+      'nanswap-api-key': apiKey
+    },
+    body: {
+      from: from,
+      to: to,
+      amount: amount,
+      toAddress: toAddress
+    },
+    json: true
+  };
+
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+
+    console.log(body);
+  });
+}
+
+function getOrder(id) {
+  const options = {
+    method: 'GET',
+    url: `https://api.nanswap.com/v1/get-order?id=${id}`,
+    json: true
+  };
+
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+
+    console.log(body);
+  });
+}
