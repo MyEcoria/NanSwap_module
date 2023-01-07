7f4c5995b3f7cc1f08f74fbf51b01c27df939d72
MyEcoria <103332030+MyEcoria@users.noreply.github.com>
2023-01-07 17:12:51 +0100

    Update setup.py

--- python/setup.py
+++ python/setup.py
@@ -1,11 +1,9 @@
 from setuptools import setup
 
 setup(name='NanSwap',
-      version='0.1',
+      version='0.4',
       description='The NanSwap Python API',
       url='https://github.com/MyEcoria/NanSwap_module',
       author='MyEcoria',
       author_email='contact@myecoria.com',
-      license='MIT',
-      packages=['NanSwap'],
-      zip_safe=False)
+      license='MIT')
